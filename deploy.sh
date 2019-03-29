f2f5247b29ca8abf32de69e41f294adc25a517f7
Jonathan Wiemers <hi@jonathanwiemers.com>
2019-03-29 16:39:06 +0100

    rebuilding site Fri Mar 29 16:39:06 CET 2019

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+git add . 
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -18,7 +15,4 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
+git push
